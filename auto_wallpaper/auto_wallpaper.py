8f5c488667b43dc9c3aa7947b7d873e7d1688aef
Rahim Hossain <rahimhossain23@gmail.com>
2022-11-27 05:46:12 +0600

    Fetch and saveap

---
+++ auto_wallpaper/auto_wallpaper.py
@@ -0,0 +1,31 @@
+""" Problem:
+    Download and change desktop wallpapers automatically 
+"""
+import requests as rq
+import json
+import PyWallpaper
+
+api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+response = rq.get(api_url)
+
+content = response.content.decode('UTF-8')
+
+#convert string to json
+
+dict_content = json.loads(content)
+
+#get image URL
+
+image_url = dict_content['hdurl']
+
+#download image
+res = rq.get(image_url)
+
+#save the image
+
+with open('./apod.png','wb') as image:
+    image.write(res.content)
+
+# set as wallpaper
+PyWallpaper.change_wallpaper('/Users/rahimhossain/Documents/codes/OOP/auto_wallpaper/apod.png')
