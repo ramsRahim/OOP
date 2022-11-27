884768a850126a17bf402d06b1025bcfddc7dbdf
Rahim Hossain <rahimhossain23@gmail.com>
2022-11-27 22:02:35 +0600

    testing in python

---
+++ my_code.py
@@ -0,0 +1,9 @@
+import requests
+
+api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+def get_response(url):
+    return requests.get(url)
+
+if __name__ == "__main__": 
+    res = get_response(api_url)
